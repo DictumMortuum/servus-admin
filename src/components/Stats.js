742a4b59c4a0d61a38c31391b5a41c98cd8a5505
DictumMortuum <theio.vrefos@gmail.com>
2021-12-30 21:40:01 +0200

    [stats] removed the exporter, since now I do not need the overloaded function, everything comes from the API. Also now using the database information for rendering the columns for the recorded stats.

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -16,7 +16,6 @@ import {
   Toolbar,
   SaveButton
 } from 'react-admin';
-import { endpoint } from '../App';
 import { JsonField } from "react-admin-json-view";
 import { useFormState } from 'react-final-form';
 import { makeStyles } from '@material-ui/core/styles';
@@ -28,61 +27,8 @@ const useStyles = makeStyles({
   },
 });
 
-async function getPlayers() {
-  const rs = await fetch(endpoint + "/rest/v1/player")
-  return rs.json()
-}
-
-async function getBoardgames() {
-  const rs = await fetch(endpoint + "/rest/v1/boardgame")
-  return rs.json()
-}
-
-async function getScores() {
-  const rs = await fetch(endpoint + "/boardgames/scores")
-  return rs.json()
-}
-
-const mapIdToPlayer = players => ({ player_id }) => players.filter(d => d.id === player_id)[0].name
-const mapIdToBoardgame = boardgames => ({ boardgame_id }) => boardgames.filter(d => d.id === boardgame_id)[0].name
-
-const parseScore = boardgames => players => score => {
-  const boardgame = mapIdToBoardgame(boardgames)(score.play)
-  const stats = score.stats.reverse().map(d => {
-    const player = mapIdToPlayer(players)(d)
-    return {
-      ...d,
-      player
-    }
-  })
-
-  return {
-    play: {
-      ...score.play,
-      boardgame
-    },
-    stats
-  }
-}
-
-const exporter = async () => {
-  let players = await getPlayers()
-  let games = await getBoardgames()
-  let scores = await getScores()
-  let retval = JSON.stringify(scores.response.map(parseScore(games)(players)), null, 2)
-  const fileName = "file";
-  const blob = new Blob([retval],{type:'application/json'});
-  const href = await URL.createObjectURL(blob);
-  const link = document.createElement('a');
-  link.href = href;
-  link.download = fileName + ".json";
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
-
 export const StatsList = props => (
-  <List {...props} exporter={exporter} perPage={25} sort={{ field: 'id', order: 'DESC' }}>
+  <List {...props} perPage={25} sort={{ field: 'id', order: 'DESC' }}>
     <Datagrid rowClick="edit">
       <TextField source="id" />
       <ReferenceField source="play_id" reference="play">
@@ -98,7 +44,6 @@ export const StatsList = props => (
       <ReferenceField source="player_id" reference="player">
         <TextField source="name" />
       </ReferenceField>
-      {/* <FunctionField label="Data" render={record => (<p>{JSON.stringify(record.data)}</p>)} /> */}
       <JsonField
         source="data"
         reactJsonOptions={{
@@ -153,23 +98,9 @@ const dateFormatter = v => {
   return `${yy}-${(pad + mm).slice(-2)}-${(pad + dd).slice(-2)}`;
 };
 
-const SelectText = props => {
-  // const { loaded, error, data } = useQueryWithStore({
-  //   type: 'getOne',
-  //   resource: 'boardgame',
-  //   payload: { id: props.boardgame_id }
-  // })
-
-  // if (!loaded) {
-  //   return <span>loading {props.boardgame_id}...</span>
-  // }
-
-  // if (error) {
-  //   return <span>failed to load {props.boardgame_id}...</span>
-  // }
-
-  return <span>{dateFormatter(new Date(props.date)) + " - " + props.boardgame}</span>
-}
+const SelectText = props => (
+  <span>{dateFormatter(new Date(props.date)) + " - " + props.boardgame}</span>
+)
 
 const OutputField = props => {
   const classes = useStyles();
@@ -201,37 +132,15 @@ const JsonInput = props => {
 const BoardgameInput = ({ className, boardgame_data, boardgame_id }) => {
   const props = { className, boardgame_data };
 
+  if (boardgame_data.hasOwnProperty("cooperative")) {
+    return <CooperativeInput {...props} />
+  }
+
   if (boardgame_data.hasOwnProperty("columns")) {
     return <DatabaseInput {...props} />
   }
 
   switch (boardgame_id) {
-    case 266810:
-      return <PaladinsOfTheWestKingdomInput {...props} />
-    case 173346:
-      return <SevenWondersDuelInput {...props} />
-    case 164928:
-      return <OrleansInput {...props} />
-    case 296151:
-      return <ViscountsInput {...props} />
-    case 193738:
-      return <GreatWesternTrailInput {...props} />
-    case 312484:
-      return <LostRuinsOfArnakInput {...props} />
-    case 216132:
-      return <ClansOfCaledoniaInput {...props} />
-    case 236457:
-      return <ArchitectsInput {...props} />
-    case 266192:
-      return <WingspanInput {...props} />
-    case 256916:
-      return <ConcordiaInput {...props} />
-    case 253499:
-      return <WarOfWhispersInput {...props} />
-    case 163412:
-      return <PatchworkInput {...props} />
-    case 170042:
-      return <RaidersInput {...props} />
     default:
       return <NumberInput source="score" {...props} />
   }
@@ -252,167 +161,8 @@ const DatabaseInput = props => (
   </div>
 )
 
-const PatchworkInput = props => (
-  <div {...props} >
-    <BooleanInput source="7x7" {...props} />
-    <NumberInput source="buttons" {...props} />
-    <NumberInput source="empty" label="Empty Spaces" {...props} />
-  </div>
-)
-
-const WarOfWhispersInput = props => (
-  <div {...props} >
-    <NumberInput source="score" {...props} />
-    <NumberInput source="swaps" {...props} />
-  </div>
-)
-
-const SevenWondersDuelInput = props => (
-  <div {...props} >
-    <NumberInput source="battle_points" label="Battle Points" {...props} />
-    <NumberInput source="blue_points" label="Blue Points" {...props} />
-    <NumberInput source="coin_points" label="Coin" {...props} />
-    <NumberInput source="green_points" label="Green Points" {...props} />
-    <NumberInput source="marker_points" label="Market Points" {...props} />
-    <NumberInput source="pantheon_points" label="Pantheon Points" {...props} />
-    <NumberInput source="purple_points" label="Purple Points" {...props} />
-    <NumberInput source="yellow_points" label="Yellow Points" {...props} />
-    <NumberInput source="wonder_points" label="Wonder Points" {...props} />
-    <BooleanInput source="battle_victory" label="Battle Victory" {...props} />
-    <BooleanInput source="science_victory" label="Science Victory" {...props} />
-  </div>
-)
-
-const PaladinsOfTheWestKingdomInput = props => (
+const CooperativeInput = props => (
   <div {...props}>
-    <NumberInput source="order" label="King's orders" {...props} />
-    <NumberInput source="black" label="Black track" {...props} />
-    <NumberInput source="blue" label="Blue track" {...props} />
-    <NumberInput source="red" label="Red track" {...props} />
-    <NumberInput source="develop" {...props} />
-    <NumberInput source="debt" {...props} />
-    <NumberInput source="coin" label="Silver/Provisions" {...props} />
-    <NumberInput source="commission" {...props} />
-    <NumberInput source="fortify" {...props} />
-    <NumberInput source="garrison" {...props} />
-    <NumberInput source="absolve" {...props} />
-    <NumberInput source="convert" {...props} />
-  </div>
-)
-
-const OrleansInput = props => (
-  <div {...props}>
-    <NumberInput source="buildings" {...props} />
-    <NumberInput source="citizen" {...props} />
-    <NumberInput source="coin" {...props} />
-    <NumberInput source="grain" {...props} />
-    <NumberInput source="cheese" {...props} />
-    <NumberInput source="wine" {...props} />
-    <NumberInput source="brocade" {...props} />
-    <NumberInput source="wool" {...props} />
-    <NumberInput source="orders" {...props} />
-    <NumberInput source="tiles" {...props} />
-  </div>
-)
-
-const ViscountsInput = props => (
-  <div {...props}>
-    <NumberInput source="build" {...props} />
-    <NumberInput source="castle" {...props} />
-    <NumberInput source="castle_leader" {...props} />
-    <NumberInput source="deeds" {...props} />
-    <NumberInput source="dept" {...props} />
-    <NumberInput source="poverty" {...props} />
-    <NumberInput source="transcribe" {...props} />
-  </div>
-)
-
-const GreatWesternTrailInput = props => (
-  <div {...props}>
-    <NumberInput source="dollars" {...props} />
-    <NumberInput source="buildings" {...props} />
-    <NumberInput source="cities" {...props} />
-    <NumberInput source="stations" {...props} />
-    <NumberInput source="hazards" {...props} />
-    <NumberInput source="cards" {...props} />
-    <NumberInput source="objectives" {...props} />
-    <NumberInput source="tasks" {...props} />
-    <NumberInput source="workers" {...props} />
-    <NumberInput source="disk" {...props} />
-    <NumberInput source="market" {...props} />
-  </div>
-)
-
-const LostRuinsOfArnakInput = props => (
-  <div {...props}>
-    <NumberInput source="cards" {...props} />
-    <NumberInput source="fear" {...props} />
-    <NumberInput source="guardian" {...props} />
-    <NumberInput source="idols" {...props} />
-    <NumberInput source="research" {...props} />
-    <NumberInput source="tiles" {...props} />
-  </div>
-)
-
-const ClansOfCaledoniaInput = props => (
-  <div {...props}>
-    <NumberInput source="bamboo" {...props} />
-    <NumberInput source="bananas" {...props} />
-    <NumberInput source="base" {...props} />
-    <NumberInput source="coin" {...props} />
-    <NumberInput source="cotton" {...props} />
-    <NumberInput source="exports" {...props} />
-    <NumberInput source="grape" {...props} />
-    <NumberInput source="manufactured" {...props} />
-    <NumberInput source="points" {...props} />
-    <NumberInput source="settlement" {...props} />
-  </div>
-)
-
-const ArchitectsInput = props => (
-  <div {...props}>
-    <NumberInput source="buildings" {...props} />
-    <NumberInput source="cathedral" {...props} />
-    <NumberInput source="debt" {...props} />
-    <NumberInput source="gold-marble" {...props} />
-    <NumberInput source="money" {...props} />
-    <NumberInput source="prison" {...props} />
-    <NumberInput source="virtue" {...props} />
-    <NumberInput source="bottom" label="End of game bottom" {...props} />
-  </div>
-)
-
-const WingspanInput = props => (
-  <div {...props}>
-    <NumberInput source="birds" {...props} />
-    <NumberInput source="bonus" {...props} />
-    <NumberInput source="eggs" {...props} />
-    <NumberInput source="endofround" {...props} />
-    <NumberInput source="food" {...props} />
-    <NumberInput source="nectar" {...props} />
-    <NumberInput source="tucked" {...props} />
-  </div>
-)
-
-const ConcordiaInput = props => (
-  <div {...props}>
-    <NumberInput source="concordia" {...props} />
-    <NumberInput source="jupiter" {...props} />
-    <NumberInput source="mars" {...props} />
-    <NumberInput source="mercurius" {...props} />
-    <NumberInput source="minerva" {...props} />
-    <NumberInput source="saturnus" {...props} />
-    <NumberInput source="venus" {...props} />
-    <NumberInput source="vesta" {...props} />
-  </div>
-)
-
-const RaidersInput = props => (
-  <div {...props}>
-    <NumberInput source="armor" {...props} />
-    <NumberInput source="loot" {...props} />
-    <NumberInput source="points" {...props} />
-    <NumberInput source="valkyrie" {...props} />
-    <NumberInput source="cards" {...props} />
+    <BooleanInput source="won" {...props} />
   </div>
 )
