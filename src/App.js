2a1cb5cbf8c08e6dec54af0f8299c0b56c341f05
DictumMortuum <theio.vrefos@gmail.com>
2022-01-02 22:29:44 +0200

    [App] wrong endpoint

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ import PersonIcon from '@material-ui/icons/Person';
 import EventIcon from '@material-ui/icons/Event';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 
-// export const endpoint = "https://servus.dictummortuum.com"
-export const endpoint = "http://localhost:1234"
+export const endpoint = "https://servus.dictummortuum.com"
+// export const endpoint = "http://localhost:1234"
 const dataProvider = simpleRestProvider(endpoint + '/rest/v1');
 
 const cacheDataProviderProxy = (dataProvider, duration = 5 * 60 * 1000) => new Proxy(dataProvider, {
