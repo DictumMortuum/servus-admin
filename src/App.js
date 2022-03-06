767f535433501aeb7b6a5509615cfd5af2296709
DictumMortuum <theio.vrefos@gmail.com>
2022-03-06 17:29:55 +0200

    [v0.2.0]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { Admin, Resource } from 'react-admin';
 import simpleRestProvider from 'ra-data-json-server';
-import { BoardgameShow, BoardgameList, BoardgameCreate } from './components/Boardgame';
+import { BoardgameShow, BoardgameList, BoardgameCreate, BoardgameEdit } from './components/Boardgame';
 import { StoreList } from './components/Store';
 import { PlayerList, PlayerCreate } from './components/Player';
 import { PlayList, PlayCreate } from './components/Play';
 import { StatsList, StatsCreate } from './components/Stats';
 import { PriceList, PriceShow, PriceEdit } from './components/Prices';
+import { MappingList, MappingCreate } from "./components/Mapping";
 import { atlasReducer, bggReducer } from './components/reducers';
 import StoreIcon from '@material-ui/icons/Store';
 import ExtensionIcon from '@material-ui/icons/Extension';
@@ -35,11 +36,12 @@ const cacheDataProviderProxy = (dataProvider, duration = 5 * 60 * 1000) => new P
 const App = () => (
   <Admin customReducers={{ atlasReducer, bggReducer }} dataProvider={cacheDataProviderProxy(dataProvider)}>
     <Resource name="store" list={StoreList} icon={StoreIcon} />
-    <Resource name="boardgame" list={BoardgameList} show={BoardgameShow} create={BoardgameCreate} icon={ExtensionIcon} />
+    <Resource name="boardgame" list={BoardgameList} show={BoardgameShow} create={BoardgameCreate} edit={BoardgameEdit} icon={ExtensionIcon} />
     <Resource name="player" list={PlayerList} create={PlayerCreate} icon={PersonIcon} />
     <Resource name="play" list={PlayList} create={PlayCreate} icon={EventIcon} />
     <Resource name="stats" list={StatsList} create={StatsCreate} icon={EventNoteIcon} />
     <Resource name="price" list={PriceList} show={PriceShow} edit={PriceEdit} />
+    <Resource name="mapping" list={MappingList} create={MappingCreate} />
   </Admin>
 );
 
